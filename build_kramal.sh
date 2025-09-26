7983ca9f163a33682ecf00f502404ad991f1b6e5
RoHaN <82774471+reaPeR1010@users.noreply.github.com>
2025-09-26 16:41:45 +0530

    WF: update

--- build_kramal.sh
+++ build_kramal.sh
@@ -20,7 +20,16 @@ if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
 fi
 COMPILER="" # llvm or default (aosp) inbuilt
 VERBOSE=0
-FULL_LTO=1
+
+# Modify defconfig
+del_config() {
+    sed -i "/^$1=y$/d" "arch/arm64/configs/$DEFCONFIG"
+}
+
+add_config() {
+    del_config "$1"
+    echo "# $1 is not set" >> "arch/arm64/configs/$DEFCONFIG"
+}
 
 # Telegram messaging function
 telegram_push() {
@@ -36,6 +45,11 @@ if [ "$COMPILER" = "llvm" ]; then
 mkdir -p clang
 wget -qO- https://www.kernel.org/pub/tools/llvm/files/llvm-21.1.2-x86_64.tar.gz | tar --strip-components=1 -xz -C clang
 PATH="${KERNEL_DIR}/clang/bin:$PATH"
+elif [ "$COMPILER" = "gcc" ]; then
+mkdir -p gcc-arm64 gcc-arm
+wget -qO- https://mirrors.edge.kernel.org/pub/tools/crosstool/files/bin/x86_64/15.2.0/x86_64-gcc-15.2.0-nolibc-aarch64-linux.tar.gz | tar --strip-components=2 -xz -C gcc-arm64
+wget -qO- https://mirrors.edge.kernel.org/pub/tools/crosstool/files/bin/x86_64/15.2.0/x86_64-gcc-15.2.0-nolibc-arm-linux-gnueabi.tar.gz | tar --strip-components=2 -xz -C gcc-arm
+PATH="${KERNEL_DIR}/gcc-arm64/bin/:${KERNEL_DIR}/gcc-arm/bin/:/usr/bin:${PATH}"
 fi
 
 # Get AnyKernel3
@@ -49,11 +63,13 @@ export KBUILD_BUILD_USER KBUILD_BUILD_HOST PROCS
 
 function compile() {
     START=$(date +"%s")
-	if [ "$FULL_LTO" = "1" ]; then
-	echo "# CONFIG_THINLTO is not set" >> arch/arm64/configs/$DEFCONFIG
-	fi
+    if [ "$COMPILER" = "gcc" ]; then
+    del_config CONFIG_TOOLS_SUPPORT_RELR
+    make O=out ARCH=arm64 $DEFCONFIG
+    make -kj"$PROCS" O=out CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- V=$VERBOSE 2>&1 | tee error.log
+    else
     make O=out ARCH=arm64 $DEFCONFIG LLVM=1
-    make -kj"$PROCS" O=out ARCH=arm64 LLVM=1 V=$VERBOSE 2>&1 | tee error.log
+    make -kj"$PROCS" O=out LLVM=1 V=$VERBOSE 2>&1 | tee error.log
 
     END=$(date +"%s")
     DIFF=$((END - START))
